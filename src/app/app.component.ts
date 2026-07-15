0a2b236634ae05ef00d1a95ad6aac0621ff83fb4
Johnny Bianco <johnnyfbianco@yahoo.com.br>
2026-07-15 10:28:07 -0300

    chore: configure initial project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { HeroComponent } from './features/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
