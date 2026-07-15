810e9e4ae68592196bfebd305b2359e0be240cd5
Johnny Bianco <johnnyfbianco@yahoo.com.br>
2026-07-15 13:26:51 -0300

    feat: create hero section - 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './core/header/header.component';
 import { HeroComponent } from './features/hero/hero.component';
+import { AboutComponent } from './features/about/about.component';
+import { SkillsComponent } from './features/skills/skills.component';
+import { ExperienceComponent } from './features/experience/experience.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { OceanComponent } from './shared/components/ocean/ocean.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,HeroComponent],
+  imports: [HeaderComponent,HeroComponent,AboutComponent,SkillsComponent,ExperienceComponent,ProjectsComponent,ContactComponent,OceanComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
